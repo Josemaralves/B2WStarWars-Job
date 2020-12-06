7653113a236c05e74d841521b91d5fa548db4f15
Josemar Alves <henriquejosemar@gmail.com>
2020-12-06 11:51:29 -0300

    Inicio da implementacao do Redis

--- src/main/java/com/josemar/redis/client/FilmRedisClient.java
+++ src/main/java/com/josemar/redis/client/FilmRedisClient.java
@@ -1,10 +1,13 @@
 package com.josemar.redis.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.swapi.model.FilmSwapi;
 import io.quarkus.redis.client.RedisClient;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.Arrays;
 
 @Singleton
 public class FilmRedisClient {
@@ -12,7 +15,13 @@ public class FilmRedisClient {
     @Inject
     RedisClient redisClient;
 
-    FilmSwapi set ()
-
+    void set(FilmSwapi film){
+        try{
+            String jsonFilm = new ObjectMapper().writeValueAsString(film);
+            redisClient.set(Arrays.asList(film.getId().toString(), jsonFilm));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/dev/swapi/model/FilmSwapi.java
+++ src/main/java/dev/swapi/model/FilmSwapi.java
@@ -6,12 +6,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class FilmSwapi {
 
     private String title;
+    private Long id;
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
 
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
 }
